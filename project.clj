8ca4d6f16abcee67dde6e8a7ac586e68c1431b1f
Dan Peddle <dan@flarework.com>
2019-09-22 18:57:09 +0200

    fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shadow-cljs-sass/lein-template "0.1.0"
   :description "Leiningen template providing a shadow cljs and sass project ready to go"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/dazld/shadow-cljs-sass"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
