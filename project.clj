24cbfdba4137f12f33140e74d7c64b7d5634fe79
Dan Peddle <dan@flarework.com>
2019-09-22 19:07:01 +0200

    prefer snapshot
    lein deployment is being weird

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shadow-cljs-sass/lein-template "0.1.0"
+(defproject shadow-cljs-sass/lein-template "0.1.0-SNAPSHOT"
   :description "Leiningen template providing a shadow cljs and sass project ready to go"
   :url "http://github.com/dazld/shadow-cljs-sass"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
