d374371f6a62cd9b912c5ecafab246c0b2d8bfc5
Dan Peddle <dan@flarework.com>
2019-09-23 22:49:52 +0200

    skip signing on clojars for releases

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject shadow-cljs-sass/lein-template "0.1.0-SNAPSHOT"
+(defproject shadow-cljs-sass/lein-template "0.1.0"
   :description "Leiningen template providing a shadow cljs and sass project ready to go"
   :url "http://github.com/dazld/shadow-cljs-sass"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories  [["clojars"  {:sign-releases false :url "https://clojars.org"}]]
+  :deploy-repositories  [["releases"  {:sign-releases false :url "https://clojars.org"}]
+                         ["snapshots"  {:sign-releases false :url "https://clojars.org"}]])
