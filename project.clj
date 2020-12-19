1ccc29e8e43fe7febe3f91049296377fb502a533
Dan Peddle <dan@flarework.com>
2020-12-19 15:03:09 +0000

    bump2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shadow-cljs-sass/lein-template "0.1.1"
+(defproject shadow-cljs-sass/lein-template "0.1.2"
   :description "Leiningen template providing a shadow cljs and sass project ready to go"
   :url "http://github.com/dazld/shadow-cljs-sass"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
